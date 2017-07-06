c7205da10eb61f71b32bb222b30cff7317e117be
Darkmind14 <eric.coughlin2014@gmail.com>
2017-07-06 15:12:06 -0400

    Rewrite keithley_laser_gather_data method and script for getting averages of photodiodes during each wavelength change.

--- bin/keithley_laser_gather_data.py
+++ bin/keithley_laser_gather_data.py
@@ -9,6 +9,6 @@ def main():
     wavelength_steps = 10
     colluminated_beam_projector = CBP()
     colluminated_beam_projector.keithley_change_wavelength_loop(wavelength_min=wavelength_min, wavelength_max=wavelength_max,
-                                                                wavelength_steps=wavelength_steps, outfile=outfile)
+                                                                wavelength_steps=wavelength_steps)
 
 main()
--- cbp/CBP/CBP.py
+++ cbp/CBP/CBP.py
@@ -14,6 +14,7 @@ import cbp.temperature
 import cbp.laser
 import numpy as np
 import thorlabs
+import os
 
 
 class CBP:
@@ -33,18 +34,40 @@ class CBP:
         #self.temperature = temperature.Temperature()
         self.laser = cbp.laser.LaserSerialInterface(loop=False)
 
-    def keithley_change_wavelength_loop(self, outfile='data.txt', wavelength_min=500, wavelength_max=700, wavelength_steps=10):
-        data_file = open(outfile,'w')
-        print("opened file")
+    def keithley_change_wavelength_loop(self, wavelength_min=500, wavelength_max=700, wavelength_steps=10):
+        shutter_closed_directory = '~/Code/cbp_2/data/wavelength_photodiode_shutter_closed/'
+        shutter_opened_directory = '~/Code/cbp_2/data/wavelength_photodiode_shutter_opened/'
+        if not os.path.exists(shutter_closed_directory):
+            os.makedirs(shutter_closed_directory)
+        if not os.path.exists(shutter_opened_directory):
+            os.makedirs(shutter_opened_directory)
         wavelength_array = np.arange(wavelength_min, wavelength_max, wavelength_steps)
         print("created array")
         for wave in wavelength_array:
+            thorlabs.thorlabs.main(val=1)
+            print("shutter closed")
+            shutter_closed_file = open(shutter_closed_directory+'{0}.dat'.format(wave), 'w')
+            print("opened file")
             print("starting change_wavelength")
             self.laser.change_wavelength(wave)
-            #photo1, photo2 = self.keithley.get_photodiode_reading()
-            thorlabs.thorlabs.main(runtype="flipper",val=1)
+            photo_diode_list = []
+            for i in range(10):
+                photo1, photo2 = self.keithley.get_photodiode_reading()
+                photo_diode_list.append(photo1)
+            photo_diode_avg = sum(photo_diode_list)/len(photo_diode_list)
+            line = "{0} {1}\n".format(wave, photo_diode_avg)
+            shutter_closed_file.write(line)
+            shutter_closed_file.close()
             thorlabs.thorlabs.main(val=2)
-            line = "{0} {1}\n".format(wave, '5.3')
-            data_file.write(line)
-        data_file.close()
+            print("shutter opened")
+            shutter_open_file = open(shutter_opened_directory + '{0}.dat'.format(wave), 'w')
+            print("opened file")
+            photo_diode_list = []
+            for i in range(10):
+                photo1, photo2 = self.keithley.get_photodiode_reading()
+                photo_diode_list.append(photo1)
+            photo_diode_avg = sum(photo_diode_list) / len(photo_diode_list)
+            line = "{0} {1}\n".format(wave, photo_diode_avg)
+            shutter_open_file.write(line)
+            shutter_open_file.close()
 
