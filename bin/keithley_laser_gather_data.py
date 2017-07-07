564e6a5aa329536839fb05a179fb28c33a52d05a
Michael Coughlin <michael.w.coughlin@gmail.com>
2017-07-07 16:43:47 +0000

    fix up script to make it work better

--- bin/keithley_laser_gather_data.py
+++ bin/keithley_laser_gather_data.py
@@ -5,8 +5,8 @@ def main():
 
     outputDir = '/home/pi/CBP/keithley/'
     wavelength_min=400
-    wavelength_max=425
-    wavelength_steps = 5
+    wavelength_max=1000
+    wavelength_steps = 10
     Naverages = 3
     collimated_beam_projector = cbp_instrument.CBP()
     collimated_beam_projector.keithley_change_wavelength_loop(outputDir=outputDir,wavelength_min=wavelength_min, wavelength_max=wavelength_max,wavelength_steps=wavelength_steps,Naverages=Naverages)
