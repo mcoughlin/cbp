fc58597d77ac4e5ce562ebac834e69dfd8f5c2e4
Darkmind14 <eric.coughlin2014@gmail.com>
2017-07-09 16:27:21 -0400

    Add some flags to cbp_class and add helper functions and more parameters

--- bin/calculate_spectograph_average.py
+++ bin/calculate_spectograph_average.py
@@ -5,7 +5,7 @@ def main():
     Naverages = 10
     duration = 10000000
     dark = False
-    collimated_beam_projector = cbp_instrument.CBP()
+    collimated_beam_projector = cbp_instrument.CBP(spectograph=True)
     collimated_beam_projector.get_spectograph_average(output_dir=output_dir, Naverages=Naverages, duration=duration, dark=dark)
 
 main()
--- bin/keithley_laser_gather_data.py
+++ bin/keithley_laser_gather_data.py
@@ -4,11 +4,12 @@ import cbp.cbp_class as cbp_instrument
 def main():
 
     outputDir = '/home/pi/CBP/keithley/'
-    wavelength_min=450
-    wavelength_max=950
+    wavelength_min = 450
+    wavelength_max = 950
     wavelength_steps = 5
     Naverages = 3
-    collimated_beam_projector = cbp_instrument.CBP()
-    collimated_beam_projector.keithley_change_wavelength_loop(outputDir=outputDir,wavelength_min=wavelength_min, wavelength_max=wavelength_max,wavelength_steps=wavelength_steps,Naverages=Naverages)
+    duration = 5000000
+    collimated_beam_projector = cbp_instrument.CBP(keithley=True,spectograph=True,laser=True)
+    collimated_beam_projector.keithley_change_wavelength_loop(outputDir=outputDir,wavelength_min=wavelength_min, wavelength_max=wavelength_max,wavelength_steps=wavelength_steps,Naverages=Naverages, duration=duration)
 
 main()
