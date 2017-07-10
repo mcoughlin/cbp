9288faed9a88f67b5a9ead28e2b122045cd820b2
Darkmind14 <eric.coughlin2014@gmail.com>
2017-07-10 15:04:28 -0400

    Add some instrumentation tests

--- bin/instrumentation_test.py
+++ bin/instrumentation_test.py
@@ -3,8 +3,29 @@ import cbp.cbp_class as CBP
 def altaz():
     cbp = CBP.CBP(altaz=True)
     raw_input("Press enter to continue")
-    cbp.altaz.do_steps(motornum=1,val=10)
-    cbp.altaz.do_steps(motornum=1, val=-10)
+    cbp.altaz.do_steps(motornum=1,val=100)
+    cbp.altaz.do_steps(motornum=1, val=-100)
+
+def birger():
+    pass
+
+def filter_wheel():
+    pass
+
+def lamp():
+    pass
+
+def phidget():
+    pass
+
+def photodiode():
+    pass
+
+def potentiometer():
+    pass
+
+def shutter():
+    pass
 
 
 def main():
